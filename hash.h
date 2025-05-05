058cc9eac6694495789c80d24d45028ecc1a030b
Raptor English <raptormenglish@gmail.com>
2025-05-04 19:16:55 -0700

    str hash

--- hash.h
+++ hash.h
@@ -20,6 +20,28 @@ struct MyStringHash {
     HASH_INDEX_T operator()(const std::string& k) const
     {
         // Add your code here
+        std::vector<HASH_INDEX_T> a(k.size());
+        for (size_t i = 0; i < k.size(); i++) {
+            a[k.size() - i - 1] = letterDigitToNumber(k[i]);
+        }
+
+        // create blocks of 6 and insert into 'w' with padding
+        int block_size = 6;
+        int max_size = 5;
+        std::vector<unsigned long long> w(max_size);
+        for (size_t i = 0; i < max_size; i++) {
+            for (size_t j = 0; j < block_size; j++) {
+                w[w.size() - i - 1] += std::pow(36, j) * a[(i * block_size) + j];
+            }
+        }
+
+        // multiply by random vector
+        unsigned long long result = 0;
+        for (size_t i = 0; i < w.size(); i++) {
+            result += rValues[i] * w[i];
+        }
+
+        return result;
 
 
     }
@@ -28,7 +50,14 @@ struct MyStringHash {
     HASH_INDEX_T letterDigitToNumber(char letter) const
     {
         // Add code here or delete this helper function if you do not want it
-
+        HASH_INDEX_T index = 0;
+        if (std::isalpha(letter)) {
+            letter = std::tolower(letter);
+            index = letter - 'a';
+        } else if (std::isdigit(letter)) {
+            index = (letter - '0') + ('z' - 'a') + 1;
+        }
+        return index;
     }
 
     // Code to generate the random R values
