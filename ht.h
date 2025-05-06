733808fc96dee0d19e2f489ec31338860a2ecea4
Raptor English <raptormenglish@gmail.com>
2025-05-05 23:50:18 -0700

    updates

--- ht.h
+++ ht.h
@@ -357,13 +357,16 @@ void HashTable<K,V,Prober,Hash,KEqual>::insert(const ItemType& p)
 //    }
 
     // overwrite value
-    if (table_[loc] != nullptr && kequal_(p.first, table_[loc]->item.first)) {
-        table_[loc]->item.second = p.second;
+    if (loc != npos) {
+        if (table_[loc] != nullptr && kequal_(p.first, table_[loc]->item.first)) {
+            table_[loc]->item.second = p.second;
 
-    } else {
-        // create new value
-        table_[loc] = new HashItem(p);
-        table_size++;
+        } else {
+            // create new value
+            table_[loc] = new HashItem(p);
+            table_size++;
+
+        }
 
     }
 
